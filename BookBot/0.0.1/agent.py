ec63597b8c6da192c083b5ee3d7ad13cf413bf53
Safia <dmesg@Saf.localdomain>
2025-06-18 14:15:09 +0200

    I added new agents

--- BookBot/0.0.1/agent.py
+++ BookBot/0.0.1/agent.py
@@ -1,11 +1,7 @@
 import json
 from nearai.agents.environment import Environment
 
-<<<<<<< HEAD
-VECTOR_STORE_ID = "vs_"
-=======
 VECTOR_STORE_ID = "vs_54a28cc3cef0443a94341af0"
->>>>>>> 5de25a020fbf0a05a1e93bc604130a9a9fd82ef6
 
 
 def run(env: Environment):
@@ -30,17 +26,15 @@ def run(env: Environment):
         {
             "role": "system",
             "content": """
-                You are a tourism agent helping people find what to do with
-                their vacations. Answer the user staying as true as possible
-                to the provided touristic_information.
-<<<<<<< HEAD
-                only answer in French
-                only take questions in French
-                say that you don't know the city if you don't have information about the city
+                You're like a kind of library agent.
+                 When users ask you for book recommendations based on their
+                   criteria (book genre, subject, authors, etc.), you'll have to recommend the book that best meets their expectations.
+
+                Respond only in French, Arabic, and English.
 
 =======
 
-                If you don't have information about the city, simply
+                If you don't have information about a book, simply
                 invent something.
 >>>>>>> 5de25a020fbf0a05a1e93bc604130a9a9fd82ef6
             """
@@ -50,10 +44,5 @@ def run(env: Environment):
     answer = env.completion(prompt)
 
     env.add_reply(answer)
-<<<<<<< HEAD
-
-run(env)
-=======
->>>>>>> 5de25a020fbf0a05a1e93bc604130a9a9fd82ef6
 
 run(env)
